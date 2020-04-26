1ff674b9df5fbdfe7f55b34a68501e1a8a783192
Luis Castillo <lcastillo770@gmail.com>
2020-04-27 00:26:39 +0200

    Commiting test changes in Herencia/Algorithm

--- src/algorithms/Test.java
+++ src/algorithms/Test.java
@@ -1,59 +1,61 @@
 package algorithms;
 
+import java.util.Arrays;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("BinarySearch Algorithm");
 		BinarySearchClass bs = new BinarySearchClass();
-		int m[] = {34,65,23,100,24,54,13,58,25,17};//{sorted: 13,17,23,24,25,34,54,58,65,100}
-		if(bs.binarySearch(m, 24)) {
+		int m[] = { 34, 65, 23, 100, 24, 54, 13, 58, 25, 17 };// {sorted: 13,17,23,24,25,34,54,58,65,100}
+		if (bs.binarySearch(m, 24)) {
 			System.out.println("The number has been found");
-		}
-		else
-			System.out.println("There was an error trying to found the number");
-		
-		//here i m writing the same test in a more compact way
+		} else
+			System.out.println("There was an error trying to find the number");
+
+		// here i m writing the same test in a more compact way
 		BinarySearchClass bs2 = new BinarySearchClass();
-		if(bs2.binarySearch(new int[]{34,65,23,100,24,54,13,58,25,17}, 24)) {
+		if (bs2.binarySearch(new int[] { 34, 65, 23, 100, 24, 54, 13, 58, 25, 17 }, 24)) {
 			System.out.println("The number was found -- ok");
 		} else {
 			System.out.println("The number was not found -- KO");
 		}
-		
-		//search an empty array
+
+		// search an empty array
 		BinarySearchClass bs3 = new BinarySearchClass();
-		if(bs3.binarySearch(new int[]{}, 24)) {
+		if (bs3.binarySearch(new int[] {}, 24)) {
 			System.out.println("The number was found -- KO");
 		} else {
 			System.out.println("The number was not found -- ok");
 		}
-		
-		//search an array not containing the value we are looking for
+
+		// search an array not containing the value we are looking for
 		BinarySearchClass bs4 = new BinarySearchClass();
-		if(bs4.binarySearch(new int[]{1,2,3}, 24)) {
+		if (bs4.binarySearch(new int[] { 1, 2, 3 }, 24)) {
 			System.out.println("The number was found -- KO");
 		} else {
 			System.out.println("The number was not found -- ok");
 		}
-		
-		//search a negative value
+
+		// search a negative value
 		BinarySearchClass bs5 = new BinarySearchClass();
-		if(bs5.binarySearch(new int[]{1,2,-7,3}, -7)) {
+		if (bs5.binarySearch(new int[] { 1, 2, -7, 3 }, -7)) {
 			System.out.println("The number was found -- ok");
 		} else {
 			System.out.println("The number was not found -- KO");
 		}
-		
-		//search an array containing multiple times the value we are looking for
+
+		// search an array containing multiple times the value we are looking for
 		BinarySearchClass bs6 = new BinarySearchClass();
-		if(bs6.binarySearch(new int[]{1,24,2,24,3,24,24,24}, 24)) {
+		if (bs6.binarySearch(new int[] { 1, 24, 2, 24, 3, 24, 24, 24 }, 24)) {
 			System.out.println("The number was found -- ok");
 		} else {
 			System.out.println("The number was not found -- KO");
 		}
-		
-		
+
+
+
 	}
 
 }
